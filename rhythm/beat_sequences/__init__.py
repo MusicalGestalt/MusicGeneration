0591923ec2ee421ce88628040f692841270b61d1
remyporter@jetpackshark.com <remyporter@jetpackshark.com>
2014-11-12 13:09:40 -0500

    Redesigned the interval generators to just emit events. Much simpler, and the inheritance model is much easier, too.

--- rhythm/beat_sequences/__init__.py
+++ rhythm/beat_sequences/__init__.py
@@ -7,51 +7,41 @@ class BaseIntervalGenerator:
     Base class for generating beat patterns. Depends on
     time signatures.
     """
-    def __init__(self, time_signature=fourfour, beat_length="quarter_note"):
+    def __init__(self, time_signature=fourfour):
         self.time_signature = time_signature
-        if isinstance(beat_length, str):
-            self.note_length = getattr(time_signature, beat_length)
-        else:
-            self.note_length = beat_length
+
+    def __iter__(self):
+        last_beat = None
+        while(True):
+            next_event = self.step(last_beat)
+            yield next_event
+            last_beat = next_event
+
+    def step(self, last_beat):
+        raise AttributeError("__step is not implemented on BaseIntervalGenerator")
 
 class DownbeatIntervalGenerator(BaseIntervalGenerator):
     """Given a time signature, this will create
     beat pattern of a down-beat at the top of the
     measure."""
-    def __init__(self, time_signature=fourfour, beat_length="quarter_note"):
-        super().__init__(time_signature, beat_length)
-
-    def __iter__(self):
-        next_event = (0, BeatEvent.note_on)
-        last_downbeat = 0
-        while (True):
-            yield next_event
-            if next_event[1] == BeatEvent.note_on: #we just emitted a down beat
-                last_downbeat = next_event[0]
-                next_event = (next_event[0] + self.note_length,
-                    BeatEvent.note_off) #so the next thing we emit 
-                                        #is an end to the note
-            else:
-                next_event = (last_downbeat + self.time_signature.ticks_per_measure,
-                    BeatEvent.note_on)
+    def __init__(self, time_signature=fourfour):
+        super().__init__(time_signature)
+    
+    def step(self, last_beat):
+        if not last_beat: return 0
+        return last_beat + self.time_signature.ticks_per_measure
 
 class MetronomeIntervalGenerator(BaseIntervalGenerator):
     """Given a time signature, this will create a note on
-    every beat."""
+    every beat. Foo"""
     def __init__(self, time_signature=fourfour, beat_length="quarter_note"):
-        super().__init__(time_signature, beat_length)
+        super().__init__(time_signature)
 
-    def __iter__(self):
-        next_event = (0, BeatEvent.note_on)
-        last_beat = 0
-        while (True):
-            yield next_event
-            if next_event[1] == BeatEvent.note_on:
-                last_beat = next_event[0]
-                next_event = (last_beat + self.note_length, BeatEvent.note_off)
-            else:
-                next_event = (last_beat + self.time_signature.ticks_per_beat,
-                    BeatEvent.note_on)
+    def step(self, last_beat):
+        if not last_beat: return 0
+        return last_beat + self.time_signature.ticks_per_beat
+
+
 
 
 
--- rhythm/beat_sequences/unit_tests.py
+++ rhythm/beat_sequences/unit_tests.py
@@ -4,51 +4,30 @@ from . import *
 from .. import (fourfour, threefour, BeatEvent)
 
 class TestDownbeatSequence(unittest.TestCase):
-    def do_test_beatgen(self, beat_gen, beat_length):
-        qn = beat_length
+    def do_test_beatgen(self, beat_gen):
         tpm = beat_gen.time_signature.ticks_per_measure
         for (index, item) in enumerate(beat_gen):
-            if item[1] == BeatEvent.note_on:
-                #down beats only happen at the start of a measure
-                self.assertEqual(item[0] % tpm, 0)
-            elif item[1] == BeatEvent.note_off:
-                #and they're exactly one beat_length long
-                self.assertEqual((item[0] - qn) % tpm, 0)
+            self.assertEqual(item % tpm, 0)
             if index > 20: break
 
     def test_fourfour(self):
-        self.do_test_beatgen(DownbeatIntervalGenerator(fourfour), 
-            fourfour.quarter_note)
-        self.do_test_beatgen(DownbeatIntervalGenerator(fourfour,"eighth_note"),
-            fourfour.eighth_note)
+        self.do_test_beatgen(DownbeatIntervalGenerator(fourfour))
 
     def test_threefour(self):
-        self.do_test_beatgen(DownbeatIntervalGenerator(threefour),
-            threefour.quarter_note)
-        self.do_test_beatgen(DownbeatIntervalGenerator(threefour,"eighth_note"),
-            threefour.eighth_note)
+        self.do_test_beatgen(DownbeatIntervalGenerator(threefour))
 
 class TestMetronome(unittest.TestCase):
-    def do_test_beatgen(self, beat_gen, beat_length):
+    def do_test_beatgen(self, beat_gen):
         tpb = beat_gen.time_signature.ticks_per_beat
         for (index, item) in enumerate(beat_gen):
-            if item[1] == BeatEvent.note_on:
-                self.assertEqual(item[0] % tpb, 0)
-            elif item[1] == BeatEvent.note_off:
-                self.assertEqual((item[0] - beat_length) % tpb, 0)
+            self.assertEqual(item % tpb, 0)
             if index > 20: break
 
     def test_fourfour(self):
-        self.do_test_beatgen(MetronomeIntervalGenerator(fourfour),
-            fourfour.quarter_note)
-        self.do_test_beatgen(MetronomeIntervalGenerator(fourfour, "eighth_note"),
-            fourfour.eighth_note)
+        self.do_test_beatgen(MetronomeIntervalGenerator(fourfour))
 
     def test_threefour(self):
-        self.do_test_beatgen(MetronomeIntervalGenerator(threefour),
-            threefour.quarter_note)
-        self.do_test_beatgen(MetronomeIntervalGenerator(threefour, "eighth_note"),
-            threefour.eighth_note)
-
+        self.do_test_beatgen(MetronomeIntervalGenerator(threefour))
+        
 def main():
     unittest.main()
