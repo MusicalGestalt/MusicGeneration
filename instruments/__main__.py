e271b1e94fa56be3f41428c58fb9f69c3c076594
Ciaran O Conaire <oconaire@gmail.com>
2014-11-15 00:50:42 -0500

    Unit tests and bug fixes for Instrument class
    Other minor changes to interfaces

--- instruments/__main__.py
+++ instruments/__main__.py
@@ -1,11 +1,7 @@
 """Instruments library. Run with: python -m MusicGeneration.instruments"""
 
+from .unit_tests import *
 
-
-def test():
-  # TODO: Add unit tests
-  pass
-
-
-test()
+print("Running main 'instrument library' code")
+main()
 
