7d43e9baa9315229fccfc8325075e092cf6eeb68
remyporter@jetpackshark.com <remyporter@jetpackshark.com>
2015-01-06 11:20:53 -0500

    Switched to PyAudio completely, ditching AudioLazy. Built a threading object to wrap the playback. There's still an issue with scaling and framerates, somehow. Have to dig into it deeper.
    Installing PyAudio remains a bitch.

--- wavefile/__init__.py
+++ wavefile/__init__.py
@@ -1,7 +1,10 @@
-import audiolazy
 import array
 import wave
+from pyaudio import PyAudio
+from collections import namedtuple
+import threading, time
 
+WaveParams = namedtuple("WaveParams", "nchannels sampwidth framerate nframes comptype compname")
 
 MONO = 1
 STEREO = 2
@@ -17,25 +20,81 @@ def scale_and_clip_data(d, scaling):
         return -scaling
     return v
 
+class PlayThread(threading.Thread):
+    def __init__(self, stream, name="PlayThread"):
+        super().__init__(name=name, target=self, daemon=True)
+        sample_width = SAMPLING_16BIT
+        self.__stream = stream
+        self.__pending = []
+        self.__stop = False
+        self.__scaling = 2 ** ((sample_width * 8) - 1)
+
+    def stop(self):
+        self.__stop = True
+        self.__stream.stop_stream()
+        self.__stream.close()
+
+    def started(self):
+        return not self.__stop
+
+    def __exit__(self):
+        self.stop()
+
+    def write(self, frames):
+        self.__pending += frames
+
+    def __call__(self):
+        while not self.__stop:
+            if len(self.__pending) > 0:
+                data = self.__pending
+                self.__pending = []
+                frames = array.array("h", [scale_and_clip_data(d, self.__scaling) for d in data])
+                self.__stream.write(frames.tobytes())
+            else:
+                time.sleep(1/1000)
+
+
 class WaveStream:
-    """Class to wrap the AudioLazy API in an object that mirrors the wavefile approach."""
+    """Class to wrap the PortAudio API in an object that mirrors the wavefile approach."""
 
     def __init__(self, filename, mode):
         self.__wavefile = wave.open(filename, mode)
-        self.__basestream = audiolazy.Stream([])
         self.__playing = False
+        self.__pyaudio = PyAudio()
+        self.__playthread = None
+        self.__basestream = None
 
-    def setparams(self, param_tuple):
-        self.__wavefile.setparams(param_tuple)
+    def __build_stream(self):
+        p = self.__pyaudio
+        params = self.__params
+        self.__basestream = p.open(format=p.get_format_from_width(params.sampwidth),
+            channels=params.nchannels,
+            rate=params.framerate,
+            output=True)
+        return self.__basestream
+
+    def __build_playthread(self):
+        if self.__playthread:
+            self.__playthread.stop()
+        self.__playthread = PlayThread(self.__build_stream())
+        self.__playthread.start()
+        return self.__playthread
+
+    def __get_playthread(self):
+        if not self.__playthread:
+            return self.__build_playthread()
+        return self.__playthread
+
+    def setparams(self, params):
+        self.__wavefile.setparams(params)
+        self.__params = WaveParams(*params)
 
     def writeframesraw(self, bytes):
-        self.__basestream.append(audiolazy.Stream(bytes))
+        self.__get_playthread().write(bytes)
         self.__wavefile.writeframesraw(bytes)
-        if not self.__playing:
-            self.play()
 
     def writeframes(self, bytes):
-        self.__basestream.append(audiolazy.Stream(bytes))
+        self.__get_playthread().write(bytes)
         self.__wavefile.writeframes(bytes)
 
     @staticmethod
@@ -44,14 +103,7 @@ class WaveStream:
 
     def close(self):
         self.__wavefile.close()
-
-    def play(self):
-        self.__playing = True
-        with audiolazy.AudioIO() as player:
-            player.play(self.__basestream)
-
-    def isplaying(self):
-        return self.__playing
+        self.__basestream.close()
 
 class WaveFile:
     """Class to facilitate simple WAV file output from float data.
